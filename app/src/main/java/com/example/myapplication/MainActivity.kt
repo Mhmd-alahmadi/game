12bca6289c5c6748821bbc58d9016fd133fc5b2d
mhmd-alahmadi <qazwsx.253403@gmail.com>
2022-04-17 14:36:05 +0300

    FRIST

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,162 @@
+package com.example.myapplication
+
+import android.annotation.SuppressLint
+import android.content.DialogInterface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
+import com.example.myapplication.databinding.ActivityMainBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+
+class MainActivity : AppCompatActivity() {
+    // view binding in project
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+
+// to select the Button and start the game
+    fun buSelect(view: View) {
+        val buChose = view as Button
+        var cellID = 0
+
+        when (buChose.id) {
+            R.id.bu1 -> cellID = 1
+            R.id.bu1 -> cellID = 2
+            R.id.bu1 -> cellID = 3
+            R.id.bu1 -> cellID = 4
+            R.id.bu1 -> cellID = 5
+            R.id.bu1 -> cellID = 6
+            R.id.bu1 -> cellID = 7
+            R.id.bu1 -> cellID = 8
+            R.id.bu1 -> cellID = 9
+        }
+
+        Play1Game(cellID, buChose)
+
+
+    }
+
+    var player1 = ArrayList<Int>()
+    var player2 = ArrayList<Int>()
+    var activePla = 1;
+    // the main fun for the game
+    fun Play1Game(cellID: Int, buChose: Button) {
+
+        if (activePla == 1) {
+            buChose.text = "X"
+            buChose.setBackgroundColor(ContextCompat.getColor(this, R.color.teal_700))
+            player1.add(cellID)
+            activePla = 2
+        } else {
+            buChose.text = "O"
+            buChose.setBackgroundColor(ContextCompat.getColor(this, R.color.purple_200))
+            player2.add(cellID)
+            activePla = 1
+        }
+
+        buChose.isEnabled = false
+        Checkwinner()
+
+
+    }
+
+    // Check winner using if condition
+    fun Checkwinner() {
+
+        var winner = -1
+        //----------------------the row win
+        if (player1.contains(1) && player1.contains(2) && player1.contains(3)) {
+            winner = 1
+        }
+        if (player2.contains(1) && player2.contains(2) && player2.contains(3)) {
+            winner = 2
+        }
+
+        //----------------------the row2 win
+        if (player1.contains(4) && player1.contains(5) && player1.contains(6)) {
+            winner = 1
+        }
+        if (player2.contains(4) && player2.contains(5) && player2.contains(6)) {
+            winner = 2
+        }
+        //-----------------------the row3 win
+        if (player1.contains(7) && player1.contains(8) && player1.contains(9)) {
+            winner = 1
+        }
+        if (player2.contains(7) && player2.contains(8) && player2.contains(9)) {
+            winner = 2
+        }
+        //-----------------------the col1 win
+        if (player1.contains(1) && player1.contains(4) && player1.contains(7)) {
+            winner = 1
+        }
+        if (player2.contains(1) && player2.contains(4) && player2.contains(7)) {
+            winner = 2
+        }
+        //-----------------------the col2 win
+        if (player1.contains(2) && player1.contains(5) && player1.contains(8)) {
+            winner = 1
+        }
+        if (player2.contains(2) && player2.contains(5) && player2.contains(8)) {
+            winner = 2
+        }
+        //-----------------------the col3 win
+        if (player1.contains(3) && player1.contains(6) && player1.contains(9)) {
+            winner = 1
+        }
+        if (player2.contains(3) && player2.contains(6) && player2.contains(9)) {
+            winner = 2
+        }
+        //-----------------------the \ win
+        if (player1.contains(1) && player1.contains(5) && player1.contains(9)) {
+            winner = 1
+        }
+        if (player2.contains(1) && player2.contains(5) && player2.contains(9)) {
+            winner = 2
+        }
+        //-----------------------the / win
+        if (player1.contains(3) && player1.contains(5) && player1.contains(7)) {
+            winner = 1
+        }
+        if (player2.contains(3) && player2.contains(5) && player2.contains(7)) {
+            winner = 2
+        }
+
+        if (winner !== -1) {
+            if (winner == 1) {
+                showAlertDialog()
+            } else {
+                showAlertDialog()
+            }
+        }
+
+    }
+    // sampie Alert for the test
+    fun showAlertDialog() {
+        val alertDialog: AlertDialog.Builder = AlertDialog.Builder(this@MainActivity)
+        alertDialog.setTitle("AlertDialog")
+        alertDialog.setMessage("Do you wanna close this Dialog?")
+        alertDialog.setPositiveButton(
+            "yes"
+        ) { _, _ ->
+            Toast.makeText(this@MainActivity, "Alert dialog closed.", Toast.LENGTH_LONG).show()
+        }
+        alertDialog.setNegativeButton(
+            "No"
+        ) { _, _ -> }
+        val alert: AlertDialog = alertDialog.create()
+        alert.setCanceledOnTouchOutside(false)
+        alert.show()
+    }
+
+
+}
